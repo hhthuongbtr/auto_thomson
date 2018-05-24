8125528f5e485eb93f38aafad1ec978a0276167b
root <hhthuongbtr@gmail.com>
2018-05-24 17:33:49 +0700

    get agent list from file

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .file import File, SupervisordFile
+from .file import File, AgentList, Agent
 from .ffmpeg import Ffmpeg
 from .DateTime import DateTime
 from .system_status import SystemStatus
--- utils/file.py
+++ utils/file.py
@@ -1,116 +1,20 @@
-import os
-from config import SUPERVISORD
+import os, json
+
+def is_json(text):
+    rt = False
+    try:
+        json.loads(text)
+        rt = True
+    except:
+        pass
+    return rt
 
 class File:
-    def __init__(self):
-        self.filedir= "/tmp/checkdata.tmp"
-        self.inprocess="/tmp/inprocess.tmp"
-        if not os.path.exists(self.filedir):
-            command="echo '\n' >"+self.filedir
-            os.system(command)
-        if not os.path.exists(self.inprocess):
-            command="echo '\n' >"+self.inprocess
-            os.system(command)
-
-    def read(self):
-        #Read file checkdata.tmp
-        f = open(self.filedir, 'r')
-        lines=f.read()
-        f.close()
-        #Clear checkdata.tmp after read
-        command="cat /dev/null > "+self.filedir
-        os.system(command)
-        #clear all and write data to inprocess file
-        command="cat /dev/null > "+self.inprocess
-        os.system(command)
-        f = open(self.inprocess, 'a')
-        f.write(lines)
-        f.close()
-        #return data
-        return lines
-
-    def append(self, text):
-        f = open(self.filedir, 'r')
-        data_rows = f.read()
-        f.close()
-        fips = open(self.inprocess, 'r')
-        data_ips = fips.read()
-        fips.close()
-        if (text not in data_rows)and (text not in data_ips):
-            f = open(self.filedir, 'a')
-            f.write(text+"\n")
-            f.close()
-            return 0
-        else:
-            print "replicate"
-            return 1
-
-class Snmp:
-    def __init__(self):
-        self.agent_channel_status = "/monitor/snmp/agent/channel_status"
-        self.agent_channel_name = "/monitor/snmp/agent/channel_name"
-        self.agent_channel_profile = "/monitor/snmp/agent/channel_profile"
-        self.analyzer_channel_status = "/monitor/snmp/analyzer/channel_status"
-        self.analyzer_channel_name = "/monitor/snmp/analyzer/channel_name"
-        self.analyzer_channel_profile = "/monitor/snmp/analyzer/channel_profile"
-
-    def read_profile(self, file_path = None):
-        file_path = self.channel_profile
-        with open(file_path, "r") as text_file:
-            return text_file.read()
-
-    def read_status(self, file_path = None):
-        file_path = self.channel_status
-        with open(file_path, "r") as text_file:
-            return text_file.read()
-
-    def read_name(self, file_path = None):
-        file_path = self.channel_name
-        with open(file_path, "r") as text_file:
-            return text_file.read()
-
-    def update_agent_profile(self, text = None):
-        f = open(self.agent_channel_profile, 'w')
-        f.write(text)
-        f.close()
-        return 0
-
-    def update_agent_status(self, text = None):
-        f = open(self.agent_channel_status, 'w')
-        f.write(text)
-        f.close()
-        return 0
-
-    def update_agent_name(self, text = None):
-        f = open(self.agent_channel_name, 'w')
-        f.write(text)
-        f.close()
-        return 0
-
-    def update_analyzer_profile(self, text = None):
-        f = open(self.analyzer_channel_profile, 'w')
-        f.write(text)
-        f.close()
-        return 0
-
-    def update_analyzer_status(self, text = None):
-        f = open(self.analyzer_channel_status, 'w')
-        f.write(text)
-        f.close()
-        return 0
-
-    def update_analyzer_name(self, text = None):
-        f = open(self.analyzer_channel_name, 'w')
-        f.write(text)
-        f.close()
-        return 0
-
-class SupervisordFile:
     def __init__(self):
         pass
 
     def read(self, filename):
-        f = open(SUPERVISORD["CONF_DIR"] + '/' + fileName, 'r')
+        f = open(filename, 'r')
         lines=f.read()
         f.close()
         return lines
@@ -120,18 +24,68 @@ class SupervisordFile:
         f.write(text)
         f.close()
 
-    def read_template(self):
-        f = open(SUPERVISORD["CONF_TEMPLATE_DIR"], 'r')
-        lines=f.read()
-        f.close()
-        return lines
-
-    def write_conf_file(self, dir, text):
-        f = open(dir, 'w')
-        f.write(text)
-        f.close()
-
     def delete(self, filename):
-        cmnd = ['/bin/rm', '-rf', SUPERVISORD["CONF_DIR"] + '/' + fileName]
+        cmnd = ['/bin/rm', '-rf', filename]
         p = subprocess.call(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(1)
+
+class AgentList:
+    def __init__(self):
+        self.filee = File()
+
+    def get(self):
+        message = "Unknow"
+        data = None
+        status = 500
+        try:
+            rsp = self.filee.read("/tmp/agent.json")
+        except Exception as e:
+            message = str(e)
+            data = None
+            status = 500
+            json_response = {"status": status, "message": message, "data": data}
+            json_response = json.dumps(json_response)
+            return json.loads(json_response)
+        if is_json(rsp):
+            status = 200
+            message = "OK"
+            try:
+                data = json.loads(rsp)
+                data = data['data']
+            except:
+                data = None
+        else:
+            message = "Data file not json fortmat"
+        json_response = {"status": status, "message": message, "data": data}
+        json_response = json.dumps(json_response)
+        return json.loads(json_response)
+
+    def seach_by_name(self, key):
+        data = self.get()
+        message = data["message"]
+        status = data["status"]
+        data = data["data"]
+        origin_list = []
+        for agent in data:
+            if key in str(agent["name"]).lower():
+                origin_list.append(agent)
+        json_response = {"status": status, "message": message, "data": origin_list}
+        json_response = json.dumps(json_response)
+        return json.loads(json_response)
+
+    def get_origin(self):
+        return self.seach_by_name("origin")
+
+    def get_4500(self):
+        return self.seach_by_name("4500")
+
+class Agent:
+    def __init__(self, ip):
+        self.al = AgentList()
+        self.ip = ip
+    def get(self):
+        data = al.get()
+
+al = AgentList()
+
+print al.get_origin()
